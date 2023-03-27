c594e7ec43fda4fef288dc61580510d57663989e
NicoDevStuff <NicoG2008@gmx.de>
2023-03-27 18:12:02 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from time import sleep
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+from ev3dev2.sound import Sound
+
+sound = Sound()
+sound.speak('Nogger ice cream haha o;jjhf;oweiohfiuweh;ofijw;lkjen;/wjihf;owhef;ouihwe;flkjndfkjhskl;djcn,.msnbfvkjhvlojknel;rkkgh;lerljihgejrhngkejrhgkejrhgkm')
