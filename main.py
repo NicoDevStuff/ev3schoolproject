8afc6ff2888cd655c06580da24a68abe78879635
neovim-hardcore-user <113374980+neovim-hardcore-user@users.noreply.github.com>
2023-03-28 09:01:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
+from ev3dev2.display import FbMem
 
+screen = FbMem()
 
 m = LargeMotor(OUTPUT_A)
+
+
+screen.draw.rectangle((10,10,60,20), fill='black')
 
 while True:
     m.on_for_rotations(SpeedPercent(100), 1)
